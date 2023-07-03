ac75432a3dfc5a12b114a38e7dd2190e892f522d
gaboDev05 <gabrielriverodev05@gmail.com>
2023-07-03 19:09:24 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hi, Gabo 🦊</h1>
+      <button>Click Here 👈🏻</button>
+      <form>
+        <input type='text' placeholder='User name'/>
+        <input type='password' placeholder='Password'/>
+      </form>
+    </>
+  )
+}
+
+export default App
