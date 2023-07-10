25ee4e602c967251720a88daad5b40385557a54d
Gabo <gabrielriveroeti@gmail.com>
2023-07-10 16:23:16 -0400

    Validacion de las props del componente

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 
 // Componente 'common' encargado de ser un campo de escritura de texto
-const TextField = ({ name, type, placeholder, required=true, state }) => {
+const TextField = ({ name, type, placeholder, required, state }) => {
     
     // Metodo que se ejecuta cada vez que cambia el valor del input debido al onChnage
     const handlerChange = (event) => { // Recibe un evento por parametro
@@ -19,6 +20,23 @@ const TextField = ({ name, type, placeholder, required=true, state }) => {
             />
         </>
     );
+}
+
+// Validacion de las props
+TextField.propTypes = {
+    name: PropTypes.string,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+    state: PropTypes.func.isRequired
+}
+
+// Valores de las props por defecto
+TextField.defaultProps = {
+    name: '',
+    type: 'Text',
+    placeholder: '',
+    required: false,
 }
 
 export default TextField;
