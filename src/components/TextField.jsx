a4310dd9a1e8572f8406da4f8536502c89581a4e
gaboDev05 <gabrielriverodev05@gmail.com>
2023-07-09 20:00:59 -0400

    Funcionamiento basico del TextField

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
 
-
-const TextField = ({ name, type, required=true }) => {
-    const [Text, setText] = useState('Hola');
+// Componente 'common' encargado de ser un campo de escritura de texto
+const TextField = ({ name, type, placeholder, required=true, state }) => {
+    
+    // Metodo que se ejecuta cada vez que cambia el valor del input debido al onChnage
+    const handlerChange = (event) => { // Recibe un evento por parametro
+        event.preventDefault() // Evita el comportamiento por defecto del input
+        state(event.target.value) // Funcion que se le pasa como prop que es la responsable de cambiar el estado en el componente padre
+    }
 
     return ( 
         <>
             <input 
+                onChange={handlerChange}
                 required={required}
                 type={type} 
                 name={name} 
-                placeholder={Text}
+                placeholder={placeholder}
             />
         </>
-     );
+    );
 }
- 
+
 export default TextField;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,24 @@
 
+import { useState } from "react";
 import TextField from "../../components/TextField";
 
 
 const Login = () => {
+    const [userName, newUserName] = useState('')
+    const [userPassword, newUserPassword] = useState('')
+
+    const printUser = () => {
+        console.log(`${userName} ${userPassword}`)
+    }
+
     return ( 
         <>
             <h1>Hi, Gabo 🦊</h1>
-            <TextField name='User' type='Text'/>
-            <TextField name='Pass' type='Password'/>
+            <TextField name='User' type='Text' placeholder='Enter your username' state={newUserName}/>
+            <TextField name='Pass' type='Password' placeholder='Enter your password' state={newUserPassword}/>
+            <button onClick={printUser}/>
         </>
-     );
+    );
 }
- 
+
 export default Login;
